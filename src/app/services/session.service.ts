b54a36fa8528842f6f78f812d0d6bd0fbade663d
Simon Ball <simonball245@gmail.com>
2023-03-29 12:24:39 +0200

    Add a character interface and a way to fetch and store characters

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, map, Observable } from 'rxjs';
+import { Character } from 'src/app/models/character';
 import { Film } from 'src/app/models/film';
 import { ApiService } from 'src/app/services/api.service';
 
@@ -8,6 +9,9 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class SessionService {
   films: BehaviorSubject<Film[]> = new BehaviorSubject<Film[]>([]);
+  characters: BehaviorSubject<Character[]> = new BehaviorSubject<Character[]>(
+    [],
+  );
   constructor(private apiService: ApiService) {
     this.apiService.get('/films').subscribe((res) => {
       this.films.next(res.results);
@@ -21,5 +25,30 @@ export class SessionService {
           films.filter((film: Film) => film.episode_id === filmId)[0],
       ),
     );
+  }
+
+  // I'm conflicted about this process because of the condition to not fetch data if I have already loaded it.
+  // I could just fetch all characters at once and store them. But in a real app this could be thousands of records and I would not want to store that much data locally.
+  // As there is not an api to fetch only the characters I want based on sending an array of id's, i'm going for sending each request when needed and storing those characters, if the characters are already stored, I won't do the request again.
+  // If the user doesn't reload the page, they will end up with all data loaded in the browser, that's not too much of an issue as there is not much data to work with.
+  getCharacters(characterUrls: string[]): void {
+    characterUrls.forEach((charUrl: string) => {
+      const hasChar = this.characters
+        .getValue()
+        .find((char) => char.url === charUrl);
+
+      if (!hasChar) {
+        const regex = /api\/(.+)/;
+        const result = charUrl.match(regex);
+        if (result) {
+          console.log(result[1]);
+          this.apiService.get(`/${result[1]}`).subscribe((res) => {
+            const chars = this.characters.getValue();
+            chars.push(res);
+            this.characters.next(chars);
+          });
+        }
+      }
+    });
   }
 }
