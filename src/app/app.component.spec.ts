b150d4c94f4881dd2ab6860450997d2cec567494
Simon Ball <simonball245@gmail.com>
2023-07-26 11:16:26 +0200

    Rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'drooms-test'`, () => {
+  it(`should have as title 'swapi-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('drooms-test');
+    expect(app.title).toEqual('swapi-test');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'drooms-test app is running!',
+      'swapi-test app is running!',
     );
   });
 });
